21dad9f9013b3b9c61991e6be4edc96daf54b5a1
Leonard <trufelnaisveni@gmail.com>
2022-01-29 20:17:18 +0300

    Added gitignore and ProgressGame

---
+++ ProgressGame/ProgressGame/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace ProgressGame
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Player1Progress = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Player2Progress = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Player1Progress
+            // 
+            this.Player1Progress.BackColor = System.Drawing.Color.Chartreuse;
+            this.Player1Progress.Location = new System.Drawing.Point(90, 76);
+            this.Player1Progress.Name = "Player1Progress";
+            this.Player1Progress.Size = new System.Drawing.Size(335, 37);
+            this.Player1Progress.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(149, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Progress Game";
+            // 
+            // Player2Progress
+            // 
+            this.Player2Progress.Location = new System.Drawing.Point(90, 153);
+            this.Player2Progress.Name = "Player2Progress";
+            this.Player2Progress.Size = new System.Drawing.Size(335, 37);
+            this.Player2Progress.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Player1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(12, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Player2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(314, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Press H for help";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(437, 214);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Player2Progress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Player1Progress);
+            this.MaximumSize = new System.Drawing.Size(453, 253);
+            this.MinimumSize = new System.Drawing.Size(453, 253);
+            this.Name = "MainForm";
+            this.Text = "Progress Game";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar Player1Progress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar Player2Progress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ProgressGame/ProgressGame/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProgressGame
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Win(int PlayerNumber) 
+        {
+            MessageBox.Show("Player " + Convert.ToString(PlayerNumber) + " is winner!");
+            Player1Progress.Value = 0;
+            Player2Progress.Value = 0;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.D: 
+                    {
+                        if (Player1Progress.Value + 3 >= 100) { Player1Progress.Value = 100;  Win(1); }
+                        else { Player1Progress.Value += 3; }
+                        break; 
+                    }
+                case Keys.A: 
+                    {
+                        if (Player2Progress.Value > 0) { Player2Progress.Value -= 1; }
+                        break; 
+                    }
+                case Keys.Right: 
+                    {
+                        if (Player2Progress.Value + 3 >= 100) { Player2Progress.Value = 100; Win(2); }
+                        else { Player2Progress.Value += 3; }
+                        break; 
+                    }
+                case Keys.Left:
+                    {
+                        if (Player1Progress.Value > 0) { Player1Progress.Value -= 1; }
+                        break; 
+                    }
+                case Keys.H:
+                    {
+                        MessageBox.Show("Player 1: D - to win, A - to interfere player 2\nPlayer 2: Right arrow - to win, Left arrow - to interfere player 1");
+                        break;
+                    }
+            }
+        }
+    }
+}
