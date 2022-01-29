6279e9a648876e035af597579b66958cb64ac264
Leonard <trufelnaisveni@gmail.com>
2022-01-29 15:50:54 +0300

    Add project NonTuchButton

---
+++ NonTuchButton/NonTuchButton/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace NonTuchButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TouchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TouchButton
+            // 
+            this.TouchButton.BackColor = System.Drawing.Color.Gold;
+            this.TouchButton.ForeColor = System.Drawing.Color.Blue;
+            this.TouchButton.Location = new System.Drawing.Point(248, 141);
+            this.TouchButton.Margin = new System.Windows.Forms.Padding(2);
+            this.TouchButton.Name = "TouchButton";
+            this.TouchButton.Size = new System.Drawing.Size(128, 33);
+            this.TouchButton.TabIndex = 0;
+            this.TouchButton.Text = "Нажми меня";
+            this.TouchButton.UseVisualStyleBackColor = false;
+            this.TouchButton.Click += new System.EventHandler(this.TouchButton_Click);
+            this.TouchButton.MouseEnter += new System.EventHandler(this.TouchButton_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(270, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::NonTuchButton.Properties.Resources._59f8e0d00c7cb__1_;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.TouchButton);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Догонялки с кнопкой";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TouchButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NonTuchButton/NonTuchButton/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NonTuchButton
+{
+    public partial class Form1 : Form
+    {
+        Point p;
+        int a, b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TouchButton_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Поздравляю, ты совершил невозможное";
+            if (p.X < TouchButton.Location.X || p.X > (TouchButton.Location.X + TouchButton.Size.Width))
+            { label1.Text = "Ввод с клавиатуры запрещён!"; }
+            if (p.Y < TouchButton.Location.Y || p.Y > (TouchButton.Location.Y + TouchButton.Height))
+            { label1.Text = "Ввод с клавиатуры запрещён!"; }
+            label1.Location = new Point(this.Width / 2 - TouchButton.Size.Width, 10);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            p = e.Location;
+        }
+
+        private void TouchButton_MouseEnter(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            Random rnd = new Random();
+            a = rnd.Next() % (this.Width - TouchButton.Size.Width);
+            b = rnd.Next() % (this.Height - TouchButton.Size.Height * 2);
+            TouchButton.Location = new Point(a, b);
+        }
+    }
+}
